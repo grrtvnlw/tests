6445cf57bec0d3e9ddaa763184ecb32b0e06b47c
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2021-09-30 09:13:21 -0400

    init

---
+++ script.test.js
@@ -0,0 +1,13 @@
+const googleSearch = require("./script");
+
+dbMock = ["dog.com", "cheesepuff.com", "disney.com", "dogpictures.com"];
+
+it("is a silly test", () => {
+  expect("hello").toBe("hello");
+  googleSearch("testtest", dbMock);
+});
+
+it("is searching google", () => {
+  expect(googleSearch("testtest", dbMock)).toEqual([]);
+  expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpictures.com"]);
+});
